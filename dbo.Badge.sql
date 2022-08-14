c95d581f7b654b43c7d3db5219b91cafe886e2b5
3003WTeam8 <111127560+3003WTeam8@users.noreply.github.com>
2022-08-14 11:56:08 +0200

    Add files via upload

---
+++ dbo.Badge.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Badge] (
+    [BadgeID]    INT            IDENTITY (1, 1) NOT NULL,
+    [Reason]     VARCHAR(250) NOT NULL,
+    [Sender]     VARCHAR(50) NOT NULL,
+    [Recipient]  VARCHAR(50) NOT NULL,
+    [EmployeeId] VARCHAR(50) NOT NULL,
+    [WalletID]   VARCHAR(50) NOT NULL,
+    [CategorySelected] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_Badge] PRIMARY KEY CLUSTERED ([BadgeID] ASC),
+    CONSTRAINT [FK_Badge_Employee_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employee] ([EmployeeId]),
+    CONSTRAINT [FK_Badge_Wallet_WalletID] FOREIGN KEY ([WalletID]) REFERENCES [dbo].[Wallet] ([WalletID])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Badge_EmployeeId]
+    ON [dbo].[Badge]([EmployeeId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Badge_WalletID]
+    ON [dbo].[Badge]([WalletID] ASC);
+
---
+++ dbo.Employee.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeId]  VARCHAR(50) NOT NULL,
+    [Name]        VARCHAR(50) NOT NULL,
+    [Surname]     VARCHAR(50) NOT NULL,
+    [Email]       VARCHAR(50) NOT NULL,
+    [Phone]       VARCHAR(10) NOT NULL,
+    [Position]    VARCHAR(50) NOT NULL,
+    [DateOfBirth] DATETIME  NOT NULL,
+    [Password]    VARCHAR(50) NOT NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
